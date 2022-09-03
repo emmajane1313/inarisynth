746bc3f817e429e40cdde59fef570266df50a0f5
devDevdesigndesign <devdesign@devdesigns-MacBook-Pro.local>
2022-09-02 23:51:09 -0400

    initialise repo + landing page

--- src/modules/landing/Landing.tsx
+++ src/modules/landing/Landing.tsx
@@ -7,7 +7,7 @@ export const Landing: FunctionComponent = (): JSX.Element => {
             <div className="w-full top-[25%] absolute">
                 <Image src="/images/icons/logoinari.png" width={100} height={144} alt="logo"/>
             </div>
-            <div className="text-distro text-offBlack top-[43%] text-9xl w-full absolute justify-center">INARI SYNTH</div>
+            <div className="text-distro text-offBlack top-1/2 sm:top-[43%] text-7xl sm:text-9xl w-full absolute justify-center">INARI SYNTH</div>
         </div>
     )
 }
