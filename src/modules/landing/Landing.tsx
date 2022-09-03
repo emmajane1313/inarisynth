c22f271180b0b6c5fef1dcaf1c36c8b9ad6762f0
devDevdesigndesign <devdesign@devdesigns-MacBook-Pro.local>
2022-09-03 00:09:59 -0400

    image priority

--- src/modules/landing/Landing.tsx
+++ src/modules/landing/Landing.tsx
@@ -5,7 +5,7 @@ export const Landing: FunctionComponent = (): JSX.Element => {
     return (
         <div className="flex text-center relative w-full min-h-screen">
             <div className="w-full top-[25%] absolute">
-                <Image src="/images/icons/logoinari.png" width={100} height={144} alt="logo"/>
+                <Image priority src="/images/icons/logoinari.png" width={100} height={144} alt="logo"/>
             </div>
             <div className="text-distro text-offBlack top-1/2 sm:top-[43%] text-7xl sm:text-9xl w-full absolute justify-center">INARI SYNTH</div>
         </div>
