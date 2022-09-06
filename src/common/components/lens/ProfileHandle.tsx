5084b8d9954d42ea35b3ba9158fa77d277efd048
devDevdesigndesign <devdesign@devdesigns-MacBook-Pro.local>
2022-09-05 20:22:36 -0400

    retest avatar ipfs

--- src/common/components/lens/ProfileHandle.tsx
+++ src/common/components/lens/ProfileHandle.tsx
@@ -14,7 +14,8 @@ export const ProfileHandle: FunctionComponent<ProfileHandleProps> = ({
       return (
         <CgProfile
           color="#00501e"
-          className="w-8 h-8 rounded-full drop-shadow-md"
+          width={10}
+          height={10}
         />
       );
     } else if (picture.original) {
@@ -22,24 +23,18 @@ export const ProfileHandle: FunctionComponent<ProfileHandleProps> = ({
         console.log("http picture")
         console.log(picture.original.url)
         return (
-          <div className="w-8 h-8 rounded-full drop-shadow-md">
-            <Image src={picture.original.url} width={"10px"} height={"10px"} />
-          </div>
+            <Image src={picture.original.url} width={10} height={10} />
         );
       } else {
         const cut = picture.original.url.split("/");
         const link = "https://lens.infura-ipfs.io/ipfs/" + cut[cut.length - 1];
         return (
-          <div className="w-8 h-8 rounded-full drop-shadow-md">
-            <Image src={link} width={"10px"} height={"10px"} />
-          </div>
+            <Image src={link} width={10} height={10} />
         );
       }
     } else {
       return (
-        <div className="w-8 h-8 rounded-full drop-shadow-md">
-          <Image src={picture.uri} width={"10px"} height={"10px"} />
-        </div>
+          <Image src={picture.uri} width={10} height={10} />
       );
     }
   };
@@ -47,11 +42,13 @@ export const ProfileHandle: FunctionComponent<ProfileHandleProps> = ({
   return (
     <button
       type="button"
-      className="m-0 cursor-pointer justify-center font-sans text-sm bg-lensLight h-10 w-fit py-1 px-2 rounded-lg hover:opacity-90 relative top-1 left-[60px]"
+      className="m-0 cursor-pointer justify-center font-space text-lensDark text-xs bg-lensLight h-10 w-fit py-1 px-2 rounded-lg hover:opacity-90 relative top-1 left-[60px]"
     >
       <div className="w-full justify-center relative flex">
-        <div>{lensProfile?.handle}</div>
+        <div>@{lensProfile?.handle}</div>
+        <div className="absolute rounded-full drop-shadow-md">
           {getAvatar(lensProfile?.picture)}
+          </div>
       </div>
     </button>
   );
