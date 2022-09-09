2b78e379202e3b8b89b3f77bdddb0f47bad48ed1
devDevdesigndesign <devdesign@devdesigns-MacBook-Pro.local>
2022-09-08 22:18:19 -0400

    build error

--- src/lib/lens/helpers.ts
+++ src/lib/lens/helpers.ts
@@ -1,9 +1,9 @@
-// import omitDeep from "omit-deep";
+import omitDeep from "omit-deep";
 import { utils } from "ethers";
 
-// export const omit = (object: any, name: string) => {
-//     return omitDeep(object, name);
-//   };
+export const omit = (object: any, name: string) => {
+    return omitDeep(object, name);
+  };
 
   export const splitSignature = (signature: any) => {
     return utils.splitSignature(signature);
--- src/modules/promptbox/hooks/useLensPost.ts
+++ src/modules/promptbox/hooks/useLensPost.ts
@@ -7,71 +7,71 @@ import {
 } from "wagmi";
 import LensHubProxy from "./../../../abis/LensHubProxy.json";
 import { LENS_HUB_PROXY_ADDRESS } from "../../../lib/lens/constants";
-import { splitSignature } from "../../../lib/lens/helpers";
+import { splitSignature, omit } from "../../../lib/lens/helpers";
 import createPostTypedData from "../../../graphql/mutations/createPost";
 import getDefaultProfile from "../../../graphql/queries/userProfile";
 
 export const useLensPost = (): useLensPostResult => {
-  // const { signTypedDataAsync } = useSignTypedData();
-  // const { address } = useAccount();
-  // const { config } = usePrepareContractWrite({
-  //   addressOrName: LENS_HUB_PROXY_ADDRESS,
-  //   contractInterface: LensHubProxy,
-  //   functionName: "postWithSig",
-  //   onError(error) {
-  //     console.log('Error', error)
-  //   },
-  // });
-  // const { writeAsync } = useContractWrite(config);
+  const { signTypedDataAsync } = useSignTypedData();
+  const { address } = useAccount();
+  const { config } = usePrepareContractWrite({
+    addressOrName: LENS_HUB_PROXY_ADDRESS,
+    contractInterface: LensHubProxy,
+    functionName: "postWithSig",
+    onError(error) {
+      console.log('Error', error)
+    },
+  });
+  const { writeAsync } = useContractWrite(config);
 
   const handlePostData = async (e: any) => {
     e.preventDefault();
-  //   try {
-  //     const profile = await getDefaultProfile(address);
-  //     console.log(profile.data.defaultProfile.id);
+    try {
+      const profile = await getDefaultProfile(address);
+      console.log(profile.data.defaultProfile.id);
 
-  //     const result = await createPostTypedData({
-  //       profileId: profile.data.defaultProfile.id,
-  //       contentURI: "https://ipfs.infura.io/ipfs/",
-  //       collectModule: {
-  //         revertCollectModule: true,
-  //       },
-  //       referenceModule: {
-  //         followerOnlyReferenceModule: false,
-  //       },
-  //     });
+      const result = await createPostTypedData({
+        profileId: profile.data.defaultProfile.id,
+        contentURI: "https://ipfs.infura.io/ipfs/",
+        collectModule: {
+          revertCollectModule: true,
+        },
+        referenceModule: {
+          followerOnlyReferenceModule: false,
+        },
+      });
 
-  //     const typedData = result.data.createPostTypedData.typedData;
+      const typedData = result.data.createPostTypedData.typedData;
 
-  //     const signature = await signTypedDataAsync({
-  //       domain: omit(typedData?.domain, "__typename"),
-  //       types: omit(typedData?.types, "__typename"),
-  //       value: omit(typedData?.value, "__typename"),
-  //     });
+      const signature = await signTypedDataAsync({
+        domain: omit(typedData?.domain, "__typename"),
+        types: omit(typedData?.types, "__typename"),
+        value: omit(typedData?.value, "__typename"),
+      });
 
-  //     const { v, r, s } = splitSignature(signature);
+      const { v, r, s } = splitSignature(signature);
 
-  //     const postArgs = {
-  //       profileId: typedData.value.profileId,
-  //       contentURI: typedData.value.contentURI,
-  //       collectModule: typedData.value.collectModule,
-  //       collectModuleInitData: typedData.value.collectModuleInitData,
-  //       referenceModule: typedData.value.referenceModule,
-  //       referenceModuleInitData: typedData.value.referenceModuleInitData,
-  //       sig: {
-  //         v,
-  //         r,
-  //         s,
-  //         deadline: typedData.value.deadline,
-  //       },
-  //     };
+      const postArgs = {
+        profileId: typedData.value.profileId,
+        contentURI: typedData.value.contentURI,
+        collectModule: typedData.value.collectModule,
+        collectModuleInitData: typedData.value.collectModuleInitData,
+        referenceModule: typedData.value.referenceModule,
+        referenceModuleInitData: typedData.value.referenceModuleInitData,
+        sig: {
+          v,
+          r,
+          s,
+          deadline: typedData.value.deadline,
+        },
+      };
 
-  //     const tx = await writeAsync?.({recklesslySetUnpreparedArgs: postArgs})
-  //     const res = await tx?.wait()
+      const tx = await writeAsync?.({recklesslySetUnpreparedArgs: postArgs})
+      const res = await tx?.wait()
 
-  //   } catch (err: any) {
-  //     console.error(err.message);
-  //   }
+    } catch (err: any) {
+      console.error(err.message);
+    }
   };
 
 
