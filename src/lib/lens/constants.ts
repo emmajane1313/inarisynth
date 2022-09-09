e8c840f078038e3ec4d5b95f59cfc82cc4499b1d
devDevdesigndesign <devdesign@devdesigns-MacBook-Pro.local>
2022-09-08 21:55:38 -0400

    apollo migration

--- src/lib/lens/constants.ts
+++ src/lib/lens/constants.ts
@@ -1,9 +1,9 @@
-export const BASE_URL: string = "https://api.lens.dev";
+export const BASE_URL: string = "https://api-mumbai.lens.dev";
 
 export const ACCESS_KEY: string = "access-token"; 
 export const REFRESH_KEY: string = "refresh-token"; 
 export const EXP: string = "exp"
 
-export const LENS_HUB_PROXY_ADDRESS = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d";
+export const LENS_HUB_PROXY_ADDRESS = "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82";
 
 export const LENS_PERIPHERY_CONTRACT = "0xD5037d72877808cdE7F669563e9389930AF404E8";
