05d610bfa8dc6940dbe7413f758e2a711bf08a33
emmajane1313 <admin@digitalax.xyz>
2022-09-23 02:06:54 -0400

    fix fouc

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-}
+  experimental: {
+    optimizeCss: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
